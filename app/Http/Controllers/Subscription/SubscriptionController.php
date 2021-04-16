e3e850ea0d6344a0d91cacedd5812bf9958d3178
Carlos Ferreira <conta@especializati.com.br>
2021-04-16 12:10:41 -0300

    Check User is Subscribed

--- app/Http/Controllers/Subscription/SubscriptionController.php
+++ app/Http/Controllers/Subscription/SubscriptionController.php
@@ -14,6 +14,9 @@ class SubscriptionController extends Controller
 
     public function index()
     {
+        if (auth()->user()->subscribed('default'))
+            return redirect()->route('subscriptions.premium');
+
         return view('subscriptions.index', [
             'intent' => auth()->user()->createSetupIntent(),
         ]);
@@ -30,6 +33,9 @@ class SubscriptionController extends Controller
 
     public function premium()
     {
+        if (!auth()->user()->subscribed('default'))
+            return redirect()->route('subscriptions.checkout');
+
         return view('subscriptions.premium');
     }
 }
