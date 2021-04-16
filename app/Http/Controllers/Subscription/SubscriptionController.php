b6d539db12f1127ba3c74bfe392a01c5d1367bc1
Carlos Ferreira <conta@especializati.com.br>
2021-04-16 10:34:50 -0300

    Create Controller and routes and views checkout

---
+++ app/Http/Controllers/Subscription/SubscriptionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Subscription;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubscriptionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
+    public function index()
+    {
+        return view('subscriptions.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->user()
+                ->newSubscription('default', 'price_1IdJnYDqbesnTmt0OlQBtobF')
+                ->create($request->token);
+
+        return redirect()->route('subscriptions.premium');
+    }
+
+    public function premium()
+    {
+        return view('subscriptions.premium');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
+use App\Http\Controllers\Subscription\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('subscriptions/checkout', [SubscriptionController::class, 'index'])->name('subscriptions.checkout');
+Route::get('subscriptions/premium', [SubscriptionController::class, 'premium'])->name('subscriptions.premium');
 
 Route::get('/', function () {
     return view('welcome');
