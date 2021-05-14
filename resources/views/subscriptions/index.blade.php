d61d25d0026ed31413369c600750ce1c72f1d56b
Carlos Ferreira <conta@especializati.com.br>
2021-05-14 11:08:15 -0300

    Validation Checkout Stripe

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                    <div id="show-errors" style="display: none;" class="mt-2 text-sm text-red-600"></div>
+
                     <p>Assinando o: {{ $plan->name }}</p>
                     <form action="{{ route('subscriptions.store') }}" method="post" id="form">
                         @csrf
@@ -45,8 +48,18 @@ const cardHolderName = document.getElementById('card-holder-name')
 const cardButton = document.getElementById('card-buttom')
 const clientSecret = cardButton.dataset.secret
 
+const showErrors = document.getElementById('show-errors')
+
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
+
+    // Disable button
+    cardButton.classList.add('cursor-not-allowed')
+    cardButton.firstChild.data = 'Validando'
+
+    // reset errors
+    showErrors.innerText = ''
+    showErrors.style.display = 'none'
 
     const { setupIntent, error } = await stripe.confirmCardSetup(
         clientSecret, {
@@ -60,8 +73,12 @@ form.addEventListener('submit', async (e) => {
     );
 
     if (error) {
-        alert('Errrrrrou')
         console.log(error)
+
+        showErrors.style.display = 'block'
+        showErrors.innerText = (error.type == 'validation_error') ? error.message : 'Dados inválidos, verifique e tente novamente!'
+
+        cardButton.classList.remove('cursor-not-allowed')
 
         return;
     }
@@ -73,5 +90,27 @@ form.addEventListener('submit', async (e) => {
     form.appendChild(token)
 
     form.submit()
+
 })
 </script>
+
+<style>
+    .StripeElement {
+        background-color: white;
+        padding: 8px 12px;
+        border-radius: 4px;
+        border: 1px solid transparent;
+        box-shadow: 0 1px 3px 0 #e6ebf1;
+        -webkit-transition: box-shadow 150ms ease;
+        transition: box-shadow 150ms ease;
+    }
+    .StripeElement--focus {
+        box-shadow: 0 1px 3px 0 #cfd7df;
+    }
+    .StripeElement--invalid {
+        border-color: #fa755a;
+    }
+    .StripeElement--webkit-autofill {
+        background-color: #fefde5 !important;
+    }
+</style>
